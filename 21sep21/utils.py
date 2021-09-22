ec00a6ef86ed1accc8618aa91dc6a8c8d49ce96a
Marcus Foo <43901768+marcusfooo@users.noreply.github.com>
2021-09-22 10:41:21 +0800

    Delete 21sep21 directory

--- 21sep21/utils.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-def convert_to_multiindex(df):
-    df = df.groupby(level=0).apply(lambda x: x.set_index('date').resample('1M').last())
-    df = df.reset_index()
-    df = df.set_index(['date','ticker']).sort_index()
-    return df
